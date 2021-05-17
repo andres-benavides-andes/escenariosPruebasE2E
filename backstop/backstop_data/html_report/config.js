eabfb53222d4e4425da73f338c48471695a9b71d
Andres Benavides <andres.benavides@eventsite.co>
2021-05-16 19:10:51 -0500

    add backstop

---
+++ backstop/backstop_data/html_report/config.js
@@ -0,0 +1,367 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../../creaar_un_post/Pruebasderegresion_Creaar_un_post_1_0_document_0_default.png",
+        "test": "../bitmaps_test/20210516-190334/Pruebasderegresion_Creaar_un_post_1_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "Pruebasderegresion_Creaar_un_post_1_0_document_0_default.png",
+        "label": "Creaar un post 1",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../tvr/3.42.5/Creaar un post/Creaar un post_1.png",
+        "referenceUrl": "../tvr/3.3.0/Creaar un post/Creaar un post_1.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 152
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../../creaar_un_post/Pruebasderegresion_Creaar_un_post_2_0_document_0_default.png",
+        "test": "../bitmaps_test/20210516-190334/Pruebasderegresion_Creaar_un_post_2_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "Pruebasderegresion_Creaar_un_post_2_0_document_0_default.png",
+        "label": "Creaar un post 2",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../tvr/3.42.5/Creaar un post/Creaar un post_2.png",
+        "referenceUrl": "../tvr/3.3.0/Creaar un post/Creaar un post_2.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 131
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../../creaar_un_post/Pruebasderegresion_Creaar_un_post_3_0_document_0_default.png",
+        "test": "../bitmaps_test/20210516-190334/Pruebasderegresion_Creaar_un_post_3_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "Pruebasderegresion_Creaar_un_post_3_0_document_0_default.png",
+        "label": "Creaar un post 3",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../tvr/3.42.5/Creaar un post/Creaar un post_3.png",
+        "referenceUrl": "../tvr/3.3.0/Creaar un post/Creaar un post_3.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 134
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../../creaar_un_post/Pruebasderegresion_Creaar_un_post_4_0_document_0_default.png",
+        "test": "../bitmaps_test/20210516-190334/Pruebasderegresion_Creaar_un_post_4_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "Pruebasderegresion_Creaar_un_post_4_0_document_0_default.png",
+        "label": "Creaar un post 4",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../tvr/3.42.5/Creaar un post/Creaar un post_4.png",
+        "referenceUrl": "../tvr/3.3.0/Creaar un post/Creaar un post_4.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "1.54",
+          "analysisTime": 113
+        },
+        "diffImage": "../bitmaps_test/20210516-190334/failed_diff_Pruebasderegresion_Creaar_un_post_4_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../../creaar_un_post/Pruebasderegresion_Creaar_un_post_5_0_document_0_default.png",
+        "test": "../bitmaps_test/20210516-190334/Pruebasderegresion_Creaar_un_post_5_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "Pruebasderegresion_Creaar_un_post_5_0_document_0_default.png",
+        "label": "Creaar un post 5",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../tvr/3.42.5/Creaar un post/Creaar un post_5.png",
+        "referenceUrl": "../tvr/3.3.0/Creaar un post/Creaar un post_5.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "4.33",
+          "analysisTime": 127
+        },
+        "diffImage": "../bitmaps_test/20210516-190334/failed_diff_Pruebasderegresion_Creaar_un_post_5_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../../creaar_un_post/Pruebasderegresion_Creaar_un_post_6_0_document_0_default.png",
+        "test": "../bitmaps_test/20210516-190334/Pruebasderegresion_Creaar_un_post_6_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "Pruebasderegresion_Creaar_un_post_6_0_document_0_default.png",
+        "label": "Creaar un post 6",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../tvr/3.42.5/Creaar un post/Creaar un post_6.png",
+        "referenceUrl": "../tvr/3.3.0/Creaar un post/Creaar un post_6.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "8.47",
+          "analysisTime": 111
+        },
+        "diffImage": "../bitmaps_test/20210516-190334/failed_diff_Pruebasderegresion_Creaar_un_post_6_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../../creaar_un_post/Pruebasderegresion_Creaar_un_post_7_0_document_0_default.png",
+        "test": "../bitmaps_test/20210516-190334/Pruebasderegresion_Creaar_un_post_7_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "Pruebasderegresion_Creaar_un_post_7_0_document_0_default.png",
+        "label": "Creaar un post 7",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../tvr/3.42.5/Creaar un post/Creaar un post_7.png",
+        "referenceUrl": "../tvr/3.3.0/Creaar un post/Creaar un post_7.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "8.15",
+          "analysisTime": 103
+        },
+        "diffImage": "../bitmaps_test/20210516-190334/failed_diff_Pruebasderegresion_Creaar_un_post_7_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../../creaar_un_post/Pruebasderegresion_Creaar_un_post_8_0_document_0_default.png",
+        "test": "../bitmaps_test/20210516-190334/Pruebasderegresion_Creaar_un_post_8_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "Pruebasderegresion_Creaar_un_post_8_0_document_0_default.png",
+        "label": "Creaar un post 8",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../tvr/3.42.5/Creaar un post/Creaar un post_8.png",
+        "referenceUrl": "../tvr/3.3.0/Creaar un post/Creaar un post_8.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "8.28",
+          "analysisTime": 101
+        },
+        "diffImage": "../bitmaps_test/20210516-190334/failed_diff_Pruebasderegresion_Creaar_un_post_8_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../../creaar_un_post/Pruebasderegresion_Creaar_un_post_9_0_document_0_default.png",
+        "test": "../bitmaps_test/20210516-190334/Pruebasderegresion_Creaar_un_post_9_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "Pruebasderegresion_Creaar_un_post_9_0_document_0_default.png",
+        "label": "Creaar un post 9",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../tvr/3.42.5/Creaar un post/Creaar un post_9.png",
+        "referenceUrl": "../tvr/3.3.0/Creaar un post/Creaar un post_9.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "8.27",
+          "analysisTime": 117
+        },
+        "diffImage": "../bitmaps_test/20210516-190334/failed_diff_Pruebasderegresion_Creaar_un_post_9_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../../creaar_un_post/Pruebasderegresion_Creaar_un_post_10_0_document_0_default.png",
+        "test": "../bitmaps_test/20210516-190334/Pruebasderegresion_Creaar_un_post_10_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "Pruebasderegresion_Creaar_un_post_10_0_document_0_default.png",
+        "label": "Creaar un post 10",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../tvr/3.42.5/Creaar un post/Creaar un post_10.png",
+        "referenceUrl": "../tvr/3.3.0/Creaar un post/Creaar un post_10.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "8.14",
+          "analysisTime": 102
+        },
+        "diffImage": "../bitmaps_test/20210516-190334/failed_diff_Pruebasderegresion_Creaar_un_post_10_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../../creaar_un_post/Pruebasderegresion_Creaar_un_post_11_0_document_0_default.png",
+        "test": "../bitmaps_test/20210516-190334/Pruebasderegresion_Creaar_un_post_11_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "Pruebasderegresion_Creaar_un_post_11_0_document_0_default.png",
+        "label": "Creaar un post 11",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../tvr/3.42.5/Creaar un post/Creaar un post_11.png",
+        "referenceUrl": "../tvr/3.3.0/Creaar un post/Creaar un post_11.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "8.14",
+          "analysisTime": 109
+        },
+        "diffImage": "../bitmaps_test/20210516-190334/failed_diff_Pruebasderegresion_Creaar_un_post_11_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../../creaar_un_post/Pruebasderegresion_Creaar_un_post_12_0_document_0_default.png",
+        "test": "../bitmaps_test/20210516-190334/Pruebasderegresion_Creaar_un_post_12_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "Pruebasderegresion_Creaar_un_post_12_0_document_0_default.png",
+        "label": "Creaar un post 12",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../tvr/3.42.5/Creaar un post/Creaar un post_12.png",
+        "referenceUrl": "../tvr/3.3.0/Creaar un post/Creaar un post_12.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "16.53",
+          "analysisTime": 107
+        },
+        "diffImage": "../bitmaps_test/20210516-190334/failed_diff_Pruebasderegresion_Creaar_un_post_12_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../../creaar_un_post/Pruebasderegresion_Creaar_un_post_13_0_document_0_default.png",
+        "test": "../bitmaps_test/20210516-190334/Pruebasderegresion_Creaar_un_post_13_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "Pruebasderegresion_Creaar_un_post_13_0_document_0_default.png",
+        "label": "Creaar un post 13",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../tvr/3.42.5/Creaar un post/Creaar un post_13.png",
+        "referenceUrl": "../tvr/3.3.0/Creaar un post/Creaar un post_13.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "16.50",
+          "analysisTime": 120
+        },
+        "diffImage": "../bitmaps_test/20210516-190334/failed_diff_Pruebasderegresion_Creaar_un_post_13_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../../creaar_un_post/Pruebasderegresion_Creaar_un_post_14_0_document_0_default.png",
+        "test": "../bitmaps_test/20210516-190334/Pruebasderegresion_Creaar_un_post_14_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "Pruebasderegresion_Creaar_un_post_14_0_document_0_default.png",
+        "label": "Creaar un post 14",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "../tvr/3.42.5/Creaar un post/Creaar un post_14.png",
+        "referenceUrl": "../tvr/3.3.0/Creaar un post/Creaar un post_14.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "16.54",
+          "analysisTime": 104
+        },
+        "diffImage": "../bitmaps_test/20210516-190334/failed_diff_Pruebasderegresion_Creaar_un_post_14_0_document_0_default.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "Pruebas de regresion"
+});
